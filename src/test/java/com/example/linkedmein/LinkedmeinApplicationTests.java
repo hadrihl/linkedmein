aef46c01bd90a303ada46fb682dd376006bc7a7c
hadrihl <hadrihl@outlook.com>
2022-10-12 09:41:33 +0800

    initial commit.

---
+++ src/test/java/com/example/linkedmein/LinkedmeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkedmein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedmeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
